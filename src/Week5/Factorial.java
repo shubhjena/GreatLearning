3c70ddcc27d84a8e2ffc0ca9fce49e2595e7744a
dexter24x7 <117462852+dexter24x7@users.noreply.github.com>
2023-01-11 09:38:12 +0530

    Recursion Practice Questions

---
+++ src/Week5/Factorial.java
@@ -0,0 +1,17 @@
+package Week5;
+
+import java.util.Scanner;
+
+public class Factorial {
+    public static void main(String[] args) {
+        System.out.print("Enter the number to calculate the factorial=");
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        System.out.println(num+"!= "+factorial(num));
+    }
+    public static int factorial(int num){
+        if(num==0||num==1)
+            return 1;
+        return num*factorial(num-1);
+    }
+}
