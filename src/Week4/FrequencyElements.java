e1d53db1eae7b04bf62ebb92c586eb97a285e3bc
dexter24x7 <117462852+dexter24x7@users.noreply.github.com>
2023-01-21 09:34:46 +0530

    FrequencyElements

---
+++ src/Week4/FrequencyElements.java
@@ -0,0 +1,27 @@
+package Week4;
+
+import java.util.Arrays;
+
+public class FrequencyElements {
+    public static void main(String[] args) {
+        int[] arr = {10, 20, 20};
+        frequencyFinder(arr);
+    }
+    static void frequencyFinder(int[] arr){
+        Arrays.sort(arr);
+        int current = arr[0],freq=0;
+        for (int i = 0; i < arr.length; i++) {
+            if(current != arr[i]){
+                System.out.println(current+": "+freq);
+                current=arr[i];
+                freq=1;
+            } else if (i== arr.length-1) {
+                System.out.println(current+": "+(++freq));
+            }
+            else if (current == arr[i]){
+                freq++;
+            }
+
+        }
+    }
+}
