7a048d850807c8a52aa6118b170ad8f3638242e2
dexter24x7 <117462852+dexter24x7@users.noreply.github.com>
2023-01-21 20:26:41 +0530

    NegativeSort

--- src/Week4/NegativeSort.java
+++ src/Week4/NegativeSort.java
@@ -8,13 +8,42 @@ public class NegativeSort {
     public static void main(String[] args) {
         int[] arr = {-12, 11, -13, -5, 6, -7, 5, -3, -6};
         negativeSeparate(arr);
+        System.out.println();
+        negativeSort(arr);
+        for (int n : arr) {
+            System.out.print(n+" ");
+        }
     }
+    //Approach 1: Print elements
     static void negativeSeparate(int[] arr){
         for (int a : arr)
             if (a < 0)
                 System.out.print(a + " ");
         for (int a : arr)
-            if (a > 0)
+            if (a >= 0)
                 System.out.print(a + " ");
+    }
+
+    //Approach 2: Swap within array
+    static void swap(int[] arr,int i, int j){
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    static void negativeSort(int[] arr){
+        int ind;
+        for (int i=0; i<arr.length-1;i++){
+            if (arr[i]>0){
+                ind=i+1;
+                while(ind< arr.length){
+                    if (arr[ind]<0) {
+                        swap(arr, i, ind);
+                        break;
+                    }
+                    ind++;
+                }
+            }
+        }
     }
 }
