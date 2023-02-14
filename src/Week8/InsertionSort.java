e1db23336b57754643b0a6c1e468ec8d8f2e21f8
dexter24x7 <117462852+shubhjena@users.noreply.github.com>
2023-02-14 23:40:21 +0530

    Insertion Sort

--- src/Week8/InsertionSort.java
+++ src/Week8/InsertionSort.java
@@ -4,7 +4,8 @@ import java.util.Arrays;
 
 public class InsertionSort {
     public static void main(String[] args) {
-        int[]  arr = {1,2,3,6,5,4,7,9,8};
+        int[]  arr = {1,-1,-2,8,-9};
+        System.out.println(Arrays.toString(arr));
         insertionSort(arr);
         System.out.println("Sorted Array: "+ Arrays.toString(arr));
 
@@ -13,14 +14,12 @@ public class InsertionSort {
     private static void insertionSort(int[] arr) {
         for (int i = 1; i < arr.length; i++) {
             int key = arr[i];
-            for (int j = i-1; j >=0 ; j--) {
-                if (arr[j]>arr[i])
-                    arr[j+1]=arr[j];
-                else {
-                    arr[j+1]=key;
-                    break;
-                }
+            int j = i-1;
+            while ( j >=0 && arr[j]>key) {
+                arr[j+1]=arr[j];
+                j--;
             }
+            arr[j+1] = key;
         }
     }
 }
