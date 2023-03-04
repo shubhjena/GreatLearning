2b1945b13881455d6b4b318e3dabdc66349bc764
dexter24x7 <117462852+shubhjena@users.noreply.github.com>
2023-03-04 17:12:31 +0530

    add Rotate Image test

---
+++ src/Week6/RotateImageTest.java
@@ -0,0 +1,37 @@
+package Week6;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RotateImageTest {
+
+    @Test
+    void Array2X2() {
+        int[][] image = {{1,2},{3,4}};
+        int[][] rotatedImage = {{3,1},{4,2}};
+        RotateImage.rotateImage(image);
+        assertArrayEquals(rotatedImage,image);
+    }
+    @Test
+    void Array3X3() {
+        int[][] image = {{1,2,3},{4,5,6},{7,8,9}};
+        int[][] rotatedImage = {{7,4,1},{8,5,2},{9,6,3}};
+        RotateImage.rotateImage(image);
+        assertArrayEquals(rotatedImage,image);
+    }
+    @Test
+    void Array4X4() {
+        int[][] image = {{1,2,3,4},{5,6,7,8},{9,10,11,12},{13,14,15,16}};
+        int[][] rotatedImage = {{13,9,5,1},{14,10,6,2},{15,11,7,3},{16,12,8,4}};
+        RotateImage.rotateImage(image);
+        assertArrayEquals(rotatedImage,image);
+    }
+    @Test
+    void voidArray() {
+        int[][] image = {};
+        int[][] rotatedImage = {};
+        RotateImage.rotateImage(image);
+        assertArrayEquals(rotatedImage,image);
+    }
+}
