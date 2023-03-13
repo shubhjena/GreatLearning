5aaba9bb3b1f06dc0d9e9ad8ce9d09a3f134f8b1
dexter24x7 <117462852+shubhjena@users.noreply.github.com>
2023-03-13 17:47:40 +0530

    update RecentCounter

--- src/Week12_Stacks_and_Queues/Practice_Problems/RecentCounter.java
+++ src/Week12_Stacks_and_Queues/Practice_Problems/RecentCounter.java
@@ -1,4 +1,14 @@
 package Week12_Stacks_and_Queues.Practice_Problems;
+//933. Number of Recent Calls
+/*
+You have a RecentCounter class which counts the number of recent requests within a certain time frame.
+
+Implement the RecentCounter class:
+
+RecentCounter() Initializes the counter with zero recent requests.
+int ping(int t) Adds a new request at time t, where t represents some time in milliseconds, and returns the number of requests that has happened in the past 3000 milliseconds (including the new request). Specifically, return the number of requests that have happened in the inclusive range [t - 3000, t].
+It is guaranteed that every call to ping uses a strictly larger value of t than the previous call.
+ */
 import java.util.*;
 
 class RecentCounter {
