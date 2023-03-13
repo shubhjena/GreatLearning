24f2028d61077ac91b435e48064f2d1c94f672c0
dexter24x7 <117462852+shubhjena@users.noreply.github.com>
2023-03-13 17:48:51 +0530

    add Built-in Queue implementation example

---
+++ src/Week12_Stacks_and_Queues/BuiltInQueue.java
@@ -0,0 +1,24 @@
+package Week12_Stacks_and_Queues;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class BuiltInQueue {
+    public static void main(String[] args) {
+        // 1. Initialize a queue.
+        Queue<Integer> q = new LinkedList<>();
+        // 2. Get the first element - return null if queue is empty.
+        System.out.println("The first element is: " + q.peek());
+        // 3. Push new element.
+        q.offer(5);
+        q.offer(13);
+        q.offer(8);
+        q.offer(6);
+        // 4. Pop an element.
+        q.poll();
+        // 5. Get the first element.
+        System.out.println("The first element is: " + q.peek());
+        // 7. Get the size of the queue.
+        System.out.println("The size is: " + q.size());
+    }
+}
