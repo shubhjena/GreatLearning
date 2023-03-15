b7903e196f057ca5c4d188d5a765edc5c54bd2cf
dexter24x7 <117462852+shubhjena@users.noreply.github.com>
2023-03-16 00:55:13 +0530

    add stack implementation using queues to week 12

--- src/Week12_Stacks_and_Queues/Practice_Problems/ImplementStackUsingQueues.java
+++ src/Week12_Stacks_and_Queues/Practice_Problems/ImplementStackUsingQueues.java
@@ -54,12 +54,3 @@ public class ImplementStackUsingQueues {
         System.out.println("Pop: "+obj.pop());
     }
 }
-
-/**
- * Your MyStack object will be instantiated and called as such:
- * MyStack obj = new MyStack();
- * obj.push(x);
- * int param_2 = obj.pop();
- * int param_3 = obj.top();
- * boolean param_4 = obj.empty();
- */
