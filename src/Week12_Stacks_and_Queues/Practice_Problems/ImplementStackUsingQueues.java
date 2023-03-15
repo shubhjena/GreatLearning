2f58b6f58d61ffc155af59c416a2f48bb6c623e8
dexter24x7 <117462852+shubhjena@users.noreply.github.com>
2023-03-16 00:54:39 +0530

    add stack implementation using queues to week 12

---
+++ src/Week12_Stacks_and_Queues/Practice_Problems/ImplementStackUsingQueues.java
@@ -0,0 +1,65 @@
+package Week12_Stacks_and_Queues.Practice_Problems;
+//225. Implement Stack using Queues
+/*
+Implement a last-in-first-out (LIFO) stack using only two queues.
+The implemented stack should support all the functions of a normal stack (push, top, pop, and empty).
+
+Implement the MyStack class:
+
+void push(int x) Pushes element x to the top of the stack.
+int pop() Removes the element on the top of the stack and returns it.
+int top() Returns the element on the top of the stack.
+boolean empty() Returns true if the stack is empty, false otherwise.
+ */
+import java.util.*;
+
+public class ImplementStackUsingQueues {
+    Queue<Integer> q1;
+    Queue<Integer> q2;
+
+    public ImplementStackUsingQueues() {
+        q1 = new LinkedList<>();
+        q2 = new LinkedList<>();
+    }
+
+    public void push(int x) {
+        while(!q1.isEmpty())
+            q2.offer(q1.poll());
+        q1.offer(x);
+        while(!q2.isEmpty())
+            q1.offer(q2.poll());
+    }
+
+    public int pop() {
+        if(empty()) return -1;
+        return q1.poll();
+    }
+
+    public int top() {
+        if(empty()) return -1;
+        return q1.peek();
+    }
+
+    public boolean empty() {
+        return q1.isEmpty();
+    }
+
+    public static void main(String[] args) {
+        ImplementStackUsingQueues obj = new ImplementStackUsingQueues();
+        obj.push(8);
+        obj.push(4);
+        obj.push(2);
+        obj.push(1);
+        System.out.println("Top: "+obj.top());
+        System.out.println("Pop: "+obj.pop());
+    }
+}
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack obj = new MyStack();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.top();
+ * boolean param_4 = obj.empty();
+ */
