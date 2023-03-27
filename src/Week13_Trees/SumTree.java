64f5d22e4c36f9392449c7c77d4233face3be8ed
shubhjena <117462852+shubhjena@users.noreply.github.com>
2023-03-28 00:29:42 +0530

    update SumTree to Trees

--- src/Week13_Trees/SumTree.java
+++ src/Week13_Trees/SumTree.java
@@ -1,6 +1,7 @@
 package Week13_Trees;
 
 public class SumTree {
+    Node root;
     static class Node{
         int data;
         Node left;
@@ -19,5 +20,39 @@ public class SumTree {
         int rightSum = (root.right != null)? getSumTree(root.right): 0;
         root.data = leftSum+rightSum;
         return nodeVal + root.data;
+    }
+    private Node addRecursively(Node current, int value){
+        if (current == null) return new Node(value);
+
+        else if (value < current.data)
+            current.left = addRecursively(current.left, value);
+        else if (value > current.data)
+            current.right = addRecursively(current.right,value);
+
+        return current;
+    }
+
+    private void add(int value){
+        addRecursively(root, value);
+    }
+    public static void printTree(Node root){
+        if (root == null)
+            return;
+        System.out.print(root.data+" ");
+        printTree(root.left);
+        printTree(root.right);
+    }
+    public static void main(String[] args) {
+        int[] treeData = new int[]{1,7,2,8,3,9,4,10,5,11,6};
+        SumTree tree = new SumTree();
+        tree.root = new Node(0);
+        for (int i: treeData){
+            tree.add(i);
+        }
+        printTree(tree.root);
+        System.out.println();
+        getSumTree(tree.root);
+        printTree(tree.root);
+
     }
 }
