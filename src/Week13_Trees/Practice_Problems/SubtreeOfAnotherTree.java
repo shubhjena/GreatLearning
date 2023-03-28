a85d8d6cbc78acb2787524003188738d8991b9bd
shubhjena <117462852+shubhjena@users.noreply.github.com>
2023-03-29 03:11:31 +0530

    add SubtreeOfAnotherTree to practice problems week 13

---
+++ src/Week13_Trees/Practice_Problems/SubtreeOfAnotherTree.java
@@ -0,0 +1,45 @@
+package Week13_Trees.Practice_Problems;
+//572. Subtree of Another Tree
+/*
+Given the roots of two binary trees root and subRoot, return true if there is a subtree of root with the same
+structure and node values of subRoot and false otherwise.
+
+A subtree of a binary tree 'tree' is a tree that consists of a node in tree and all of this node's descendants.
+The tree 'tree' could also be considered as a subtree of itself.
+ */
+
+public class SubtreeOfAnotherTree {
+    public static boolean isSubtree(TreeNode root, TreeNode subRoot) {
+        if(root ==  null || subRoot ==null) return (subRoot== null)&&(root==null);
+        if(root.val == subRoot.val){
+            if(isSubtreeHelper(root,subRoot)) return true;
+        }
+        return (isSubtree(root.left,subRoot) || isSubtree(root.right,subRoot) );
+    }
+    public static boolean isSubtreeHelper(TreeNode root, TreeNode subRoot) {
+        if(root ==  null || subRoot ==null) return (subRoot== null)&&(root==null);
+        if(root.val == subRoot.val){
+            return (isSubtreeHelper(root.left,subRoot.left) &&
+                    isSubtreeHelper(root.right,subRoot.right) );
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        BinaryTree tree = new BinaryTree();
+        int[] input = new int[]{1,2};
+        tree.root = new TreeNode(input[0]);
+        for (int i=1;i< input.length; i++){
+          tree.insertTreeNode(tree.root,input[i]);
+        }
+        BinaryTree subtree = new BinaryTree();
+        int[] inputSubtree = new int[]{1,2};
+        subtree.root = new TreeNode(inputSubtree[0]);
+        for (int i=1;i< input.length; i++){
+            subtree.insertTreeNode(subtree.root,inputSubtree[i]);
+        }
+        boolean isPresent = isSubtree(tree.root,subtree.root);
+        System.out.println(isPresent);
+    }
+}
+
