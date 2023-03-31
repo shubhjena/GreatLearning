b0584ad1c5e0cc236629c83f6d681d586ca6c3dd
shubhjena <117462852+shubhjena@users.noreply.github.com>
2023-03-31 23:54:32 +0530

    add symmetric tree to practice problems week 13

---
+++ src/Week13_Trees/Practice_Problems/SymmetricTree.java
@@ -0,0 +1,29 @@
+package Week13_Trees.Practice_Problems;
+
+public class SymmetricTree {
+    static boolean flag = true;
+    public static boolean isSymmetric(TreeNode root) {
+        inOrderTraversal(root.left, root.right);
+        return flag;
+    }
+    private static void inOrderTraversal(TreeNode leftSide, TreeNode rightSide){
+        if (leftSide == null && rightSide == null) return;
+        if (leftSide == null || rightSide == null){
+            flag = false;
+            return;
+        }
+        inOrderTraversal(leftSide.left,rightSide.right);
+        if(leftSide.val != rightSide.val) flag = false;
+        inOrderTraversal(leftSide.right,rightSide.left);
+    }
+
+    public static void main(String[] args) {
+        BinaryTree tree = new BinaryTree();
+        int[] input = new int[]{1,2,2,3,4,4,3};
+        tree.root = new TreeNode(input[0]);
+        for (int i=1;i< input.length; i++){
+            tree.insertTreeNode(tree.root,input[i]);
+        }
+        System.out.println("Is the binary tree symmetric?\n"+ isSymmetric(tree.root));
+    }
+}
