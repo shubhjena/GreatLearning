5708fc7dfc026f027bf44a7c1d97a277aef4b6a1
shubhjena <117462852+shubhjena@users.noreply.github.com>
2023-04-08 21:06:50 +0530

    update MajorityElement to week 11 Hashing

--- src/Week11_Hashing_and_Bit_manipulation/Hashing/MajorityElement.java
+++ src/Week11_Hashing_and_Bit_manipulation/Hashing/MajorityElement.java
@@ -1,5 +1,10 @@
 package Week11_Hashing_and_Bit_manipulation.Hashing;
-
+//169. Majority Element
+/*
+Given an array nums of size n, return the majority element.
+The majority element is the element that appears more than ⌊n / 2⌋ times.
+You may assume that the majority element always exists in the array.
+ */
 public class MajorityElement {
     public static int majorityElement(int[] nums) {
         //Boyer–Moore majority vote algorithm
