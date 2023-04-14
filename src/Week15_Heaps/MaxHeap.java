b7c536fde61af61b42d52b0fc303a9459849e128
shubhjena <117462852+shubhjena@users.noreply.github.com>
2023-04-14 23:59:33 +0530

    add MaxHeap

--- src/Week15_Heaps/MaxHeap.java
+++ src/Week15_Heaps/MaxHeap.java
@@ -1,15 +1,32 @@
 package Week15_Heaps;
 
 public class MaxHeap {
-    private final int[] heap; // array
-    private final int size;
-    private final int maxsize;
+    private int[] heap; // array
+    private int size;
+    private int maxsize;
 
     public MaxHeap(int size) {
         this.maxsize = size;
         this.size = 0;
         heap = new int[this.maxsize + 1];
         heap[0] = Integer.MAX_VALUE;
+    }
+
+    public void heapify(int num){
+        if(size == maxsize) return;
+        size++;
+        if (size == 1){
+            heap[0] = num;
+            return;
+        }
+        int parent = (size-2)/2;
+        if (parent > 0 && heap[size-1]>heap[parent]){
+            int temp = heap[size-1];
+            heap[size-1] = heap[parent];
+            heap[parent] = temp;
+            heapify(heap[parent]);
+        }
+
     }
 
     public static void main(String[] args) {
