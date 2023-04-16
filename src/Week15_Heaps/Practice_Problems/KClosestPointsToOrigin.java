ba8a90c35e395f325c02e381c5cbfbe88fff78f0
shubhjena <117462852+shubhjena@users.noreply.github.com>
2023-04-17 00:41:20 +0530

    add KClosestPointsToOrigin to week 15 heap practice problems

---
+++ src/Week15_Heaps/Practice_Problems/KClosestPointsToOrigin.java
@@ -0,0 +1,36 @@
+package Week15_Heaps.Practice_Problems;
+
+import java.util.Arrays;
+import java.util.PriorityQueue;
+
+public class KClosestPointsToOrigin {
+    public static int[][] kClosest(int[][] points, int k) {
+        //queue to store the points in a heap based on distance from origin
+        PriorityQueue<Integer[]> queue = new PriorityQueue<>(KClosestPointsToOrigin::distance);
+        for(int[] point : points){
+            queue.add(new Integer[]{point[0], point[1]});
+        }
+
+        //res to store k closest points
+        int[][] res = new int[k][2];
+
+        //first k points in the priority queue are the required points
+        for(int i=0; i<k && !queue.isEmpty() ; i++){
+            res[i] = new int[]{queue.peek()[0],queue.poll()[1]};
+        }
+        return res;
+    }
+    //returns the difference of distances of two points from origin
+    static int distance(Integer[] x,Integer[] y){
+        int xd =  (int)(Math.pow(x[0],2) + Math.pow(x[1],2));
+        int yd =  (int)(Math.pow(y[0],2) + Math.pow(y[1],2));
+        return xd-yd;
+    }
+
+    public static void main(String[] args) {
+        int[][] points = new int[][]{ {2,10},{-9,-9},{0,8},{-2,-2},{8,9},{-10,-7},{-5,2},{-4,-9}};
+        int k = 7;
+        int[][] res = kClosest(points,k);
+        System.out.println(Arrays.deepToString(res));
+    }
+}
