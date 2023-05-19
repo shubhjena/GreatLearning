dcae915ad02a74557e9d58afa35ff3f70a28d0f9
shubhjena <117462852+shubhjena@users.noreply.github.com>
2023-05-19 23:58:24 +0530

    add main to week 18

---
+++ src/Week18_Graphs/Main.java
@@ -0,0 +1,23 @@
+package Week18_Graphs;
+
+public class Main {
+    public static void main(String[] args) {
+        AdjacencyListGraph graph = new AdjacencyListGraph(4);
+        Vertex v1 = new Vertex("A");
+        Vertex v2 = new Vertex("B");
+        Vertex v3 = new Vertex("C");
+        Vertex v4 = new Vertex("D");
+
+        graph.getVertices()[0] = v1;
+        graph.getVertices()[1] = v2;
+        graph.getVertices()[2] = v3;
+        graph.getVertices()[3] = v4;
+
+        graph.addNeighbour(v1,v2);
+        graph.addNeighbour(v2,v3);
+        graph.addNeighbour(v1,v3);
+        graph.addNeighbour(v3,v4);
+
+        graph.printAllEdges();
+    }
+}
