e06e4784ced5b694127ab4378a1677bae2e8e72b
shubhjena <117462852+shubhjena@users.noreply.github.com>
2023-05-23 01:01:06 +0530

    add DFSTraversal to week 18: add stack implementation of DFS traversal to the class

--- src/Week18_Graphs/DFSTraversal.java
+++ src/Week18_Graphs/DFSTraversal.java
@@ -1,23 +1,28 @@
 package Week18_Graphs;
 
 import java.util.List;
+import java.util.Stack;
 
 public class DFSTraversal {
+    private final Stack<Vertex> stack = new Stack<>();
+
     /**
      * If we have a cluster of unconnected graphs
-     * @param vertexList
+     * @param vertexList: list of root vertices of independent clusters of graph
      */
     public void dfsTraversal(List<Vertex> vertexList){
         for (Vertex vertex : vertexList){
             if (!vertex.isVisited()){
                 vertex.setVisited(true);
+                //choose either the recursive or the stack implementation
                 dfsRec(vertex);
+//                dfsStack(vertex);
             }
         }
     }
     /**
      * this function traverses graph by DFS using recursion
-     * @param root
+     * @param root: start node of graph
      */
     public void dfsRec(Vertex root){
 
@@ -29,6 +34,27 @@ public class DFSTraversal {
             if (!neighbour.isVisited()){
                 neighbour.setVisited(true);
                 dfsRec(neighbour);
+            }
+        }
+    }
+
+    /**
+     * this function traverses graph by DFS using Stack data structure
+     * @param root: start node of graph
+     */
+    public void dfsStack(Vertex root){
+        this.stack.push(root);
+        root.setVisited(true);
+
+        while(!this.stack.isEmpty()){
+            Vertex current = this.stack.pop();
+            System.out.println("visited: "+current);
+
+            for (Vertex v: current.getAdjacencyList()){
+                if (!v.isVisited()){
+                    v.setVisited(true);
+                    this.stack.push(v);
+                }
             }
         }
     }
