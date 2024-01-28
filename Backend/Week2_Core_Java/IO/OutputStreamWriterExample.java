e882389effd2425846f5a756779beccf3b09c539
shubhjena <117462852+shubhjena@users.noreply.github.com>
2024-01-28 20:37:08 +0530

    add OutputStreamWriterExample to Week2

--- Backend/Week2_Core_Java/IO/OutputStreamWriterExample.java
+++ Backend/Week2_Core_Java/IO/OutputStreamWriterExample.java
@@ -1,4 +1,23 @@
 package Week2_Core_Java.IO;
 
+import java.io.*;
+
 public class OutputStreamWriterExample {
+    public static void main(String[] args) {
+        String fileName = "output.txt";
+
+        try (OutputStream outputStream = new FileOutputStream(fileName);
+             Writer writer = new OutputStreamWriter(outputStream, "UTF-8")) {
+
+            // Writing characters to the file using OutputStreamWriter
+            writer.write("Hello, World!\n");
+            writer.write("This is an example of OutputStreamWriter.");
+
+            System.out.println("Data has been written to " + fileName);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+
+        }
+    }
 }
